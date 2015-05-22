26b35229a8d72d74f9fd2955a67a47bafd0dd1a6
JuanKAlvarez <JuanAlvarezCuartas@hotmail.com>
2015-05-22 15:23:34 -0500

    Lang es
    Se ingresa una traduccion al Español a la  mayoria de la aplicacion

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>Error!</strong>  Hubo algunos problemas con su Ingreso.<br><br>
+							<strong>Error!</strong>  Hubo algunos problemas con su Registro.<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
 									<li>{{ $error }}</li>
